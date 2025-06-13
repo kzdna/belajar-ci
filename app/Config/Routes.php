35293efefce604cfa84635ee9b48241af27701eb
Salsa Dharma Arindina <1112023145246@mhs.dinus.ac.id>
2025-06-13 15:29:55 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -28,6 +28,7 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
 });
 
 $routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
 
 $routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
 $routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
